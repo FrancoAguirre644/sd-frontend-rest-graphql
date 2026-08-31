229908e201a1c8fc4b1288e37d16cb7726890dc5
francoaguirre644 <francoaguirre644@gmail.com>
2026-08-31 00:02:29 -0300

    add Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
+import {
+  AppBar,
+  Button,
+  Container,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+
 function App() {
   return (
-    <main>
-      <h1>Vehicles</h1>
-      <p>REST and GraphQL API client</p>
-    </main>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            Vehicles
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container sx={{ mt: 4 }}>
+        <Typography variant="h4" gutterBottom>
+          Vehicles
+        </Typography>
+
+        <Typography variant="body1" sx={{ mb: 2 }}>
+          REST and GraphQL API client
+        </Typography>
+
+        <Button variant="contained">
+          Test MUI
+        </Button>
+      </Container>
+    </>
   );
 }
 
